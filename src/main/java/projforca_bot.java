57eb478371daa050edccbb104d08aa619cfdfa49
ladp17 <leonardoadpereira@gmail.com>
2019-07-19 20:49:51 -0300

    projeto aberto. funciona. bot inicializa só fazer a forca agr.

---
+++ src/main/java/projforca_bot.java
@@ -0,0 +1,18 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class projforca_bot extends TelegramLongPollingBot {
+
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return "projforca_bot";
+    }
+
+    public String getBotToken() {
+        return "978041427:AAFlTM06U6zD4_-9Ske_LuFa8G9VjaNRCS0";
+    }
+}
